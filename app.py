301da59d3503bdcc0acaf4288d602eaea3822a52
VincentGvr <vincentguyonvarch1995@gmail.com>
2023-11-07 18:37:48 +0100

    toto

--- app.py
+++ app.py
@@ -10,24 +10,24 @@ app = Flask(__name__)
 def index():
    print('Request for index page received')
    return render_template('index.html') # render_template uses the files in the templates folder
-#
-#@app.route('/favicon.ico')
-#def favicon():
-#    return send_from_directory(os.path.join(app.root_path, 'static'),
-#                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-#
-#@app.route('/hello', methods=['POST'])
-#def hello():
-#   name = request.form.get('name')
-#
-#   if name:
-#       print('Request for hello page received with name=%s' % name)
-#       return render_template('hello.html', name = name)
-#   else:
-#       print('Request for hello page received with no name or blank name -- redirecting')
-#       return redirect(url_for('index'))
-#
-#
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run()
     #app.run(debug=True)
